1196b9f11c238fe3b0dff2e3ad7fcbac62bf2c1d
Kent <le6790@gmail.com>
2018-04-20 01:18:19 -0600

    Initial commit. Amazon and ebay scrapers

---
+++ amazonPageScraper.py
@@ -0,0 +1,80 @@
+from lxml import html
+import os
+import requests
+import unicodecsv as csv
+from time import sleep
+from traceback import format_exc
+import argparse
+from pprint import pprint
+
+def getAmazonPageData(url):
+	#spoof Headers
+	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+
+	response = requests.get(url, headers=headers)
+
+	if (response.status_code != 200):
+		print("Bad Response: " + response.status_code)
+
+	print("Parsing: " + url)
+
+	pageData = html.fromstring(response.content)
+
+	product_listings = pageData.xpath("//div[contains(@class,'s-result-list')]")
+	print("length of product_listings: ", len(product_listings))
+	scraped_products = []
+	for product in product_listings:
+		raw_price = product.xpath('.//li[contains(@class,"s-result-item")]//div[contains(@class, "s-item-container")]//div[contains(@class,"a-row")]//a[contains(@class,"a-text-normal")]//span[contains(@class,"a-offscreen")]/text()')
+		raw_title = product.xpath('.//li[contains(@class,"s-result-item")]//div[contains(@class, "s-item-container")]//div[contains(@class,"a-row")]//a[contains(@class,"a-link-normal")]//h2[contains(@class,"s-access-title")]/text()')
+		#print("RawPrice: ", raw_price)
+		for i in raw_price:
+			try:
+				raw_price.remove('[Sponsored]')
+			except ValueError:
+				pass
+		for i in raw_title:
+			try:
+				raw_title.remove('[Sponsored]')
+			except ValueError:
+				pass
+		# for x in range(0, len(raw_price)):
+		# 	if(x == '[Sponsored]'):
+		# 		raw_price.remove('[Sponsored]')
+		price = (' '.join(raw_price).split())
+		title = ' '.join(' '.join(raw_title).split())
+		#print(price)
+		count =0
+		for x in raw_price:
+
+			data = {
+			'title':raw_title[count],
+			'price':raw_price[count]
+			}
+			count = count +1
+			scraped_products.append(data)
+		# data = {
+		# 	'title':title,
+		# 	'price':price
+		# }
+		#scraped_products.append(data)
+	print("Title Length: " + str(len(raw_title)))
+	print("Price lenth: " + str(len(raw_price)))
+	return scraped_products
+
+def makeCsvFile(scraped_data, item):
+	with open('%samazon-data.csv'%(item),'wb') as csvfile:
+		fieldnames = ["title","price",]
+		writer = csv.DictWriter(csvfile, fieldnames = fieldnames, quoting=csv.QUOTE_ALL)
+		writer.writeheader()
+		for data in scraped_data:
+			writer.writerow(data)
+
+def beginAmazonScrape(item):
+	itemUrl = "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords="+item
+	scraped_data = getAmazonPageData(itemUrl)
+	makeCsvFile(scraped_data, item)
+# if __name__ == "__main__":
+
+# 	scraped_data = getAmazonPageData("https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=htc one m9")
+# 	makeCsvFile(scraped_data)
+
---
+++ onlineScraper.py
@@ -0,0 +1,122 @@
+from lxml import html
+import requests
+from pprint import pprint
+import unicodecsv as csv
+from traceback import format_exc
+import argparse
+import sqlite3
+
+import amazonPageScraper as aps
+
+
+def parse(brand):
+	#Test for
+	
+	try:
+
+		url = 'http://www.ebay.com/sch/i.html?_nkw={0}&_sacat=0'.format(brand)
+		headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+		
+		#get the page html data
+		response = requests.get(url, headers=headers, verify=False)
+		
+		#print(type(response.status_code))
+		#Check the response
+		if(response.status_code != 200):
+		
+			print("Bad response: " + response.status_code)		
+		print("Parsing: "+ url)
+
+		pageData = html.fromstring(response.content)
+		print(type(pageData))
+
+		product_listings = pageData.xpath('//li[contains(@class,"lvresult")]')
+		raw_result_count = pageData.xpath("//span[@class='rcnt']//text()")
+		print(product_listings)
+		result_count = ''.join(raw_result_count).strip()
+
+
+		print("Found {0} results for {1}".format(result_count,brand))
+
+		scraped_products = [] # list of scraped products
+		#add parsed data to scraped_products
+		for product in product_listings:
+			raw_url = product.xpath('.//a[@class="vip"]/@href')
+			#print(raw_url)
+			raw_title = product.xpath('.//a[@class="vip"]/text()')
+			#raw_price = pageData.xpath(".//span[@class='prRange']//span[@class='bold']//text()")	
+			raw_price = product.xpath(".//li[contains(@class,'lvprice')]//span[@class='bold']//text()")
+			#print(raw_title)
+			#print(raw_price)
+			title = ' '.join(' '.join(raw_title).split())
+			price = ' '.join(' '.join(raw_price).split())
+			print("Rawprice: ", raw_price)
+			print("price: ", price)
+			data = {
+				
+				'title':title,
+				'price':price
+			}
+			scraped_products.append(data)
+			addToDB(title, price)
+		return scraped_products
+
+
+	except Exception as e:
+		print (e)	
+
+
+def addToDB(title, price):
+	conn = sqlite3.connect('sellers.db')
+	c = conn.cursor()
+
+	def create_table():
+		c.execute("CREATE TABLE IF NOT EXISTS amazon(item TEXT, value TEXT)")
+
+	# def data_entry():
+	# 	c.execute("INSERT INTO amazon VALUES('watches', '$19.54')")
+	# 	conn.commit()
+	# 	c.close()
+	# 	conn.close()
+
+	create_table()
+	c.execute("INSERT INTO amazon VALUES(title, price) VALUES (?, ?)")
+	conn.commit()
+
+	c.close()
+	conn.close()
+
+
+def makeCsvFile(scraped_data):
+	with open('%s-ebay-data.csv'%(brand),'wb') as csvfile:
+		fieldnames = ["title","price",]
+		writer = csv.DictWriter(csvfile, fieldnames = fieldnames, quoting=csv.QUOTE_ALL)
+		writer.writeheader()
+		for data in scraped_data:
+			writer.writerow(data)
+
+if __name__ == "__main__":
+	print("Currently in Main.")
+
+	userInp = input("a) Amazon or b) Ebay?")
+	brand = ''
+	if userInp =="a":
+		brand = input("Enter Brand Name: ")
+		aps.beginAmazonScrape(brand)
+	elif userInp == "b":
+		brand =  input("Enter Brand name: ")
+		scraped_data = parse(brand)
+		makeCsvFile(scraped_data)
+		#addToDB(scraped_data)
+	else:
+		exit()
+
+	
+	# argparser = argparse.ArgumentParser()
+	# argparser.add_argument('brand', help = 'Brand Name')
+	# args = argparser.parse_args()
+ 
+	#print(brand)
+
+
+	#print(scraped_data)
